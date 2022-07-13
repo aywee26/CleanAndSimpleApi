8344bac428040008b72d62f3adf927ee764ccc17
WOTTAKWOT <aywee26@gmail.com>
2022-07-13 22:13:24 +0300

    Move WeatherForecast to Domain.

---
+++ src/Domain/Entities/WeatherForecast.cs
@@ -0,0 +1,10 @@
+namespace Domain;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public string? Summary { get; set; }
+}
