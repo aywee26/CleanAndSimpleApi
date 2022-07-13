aef26d3841f7d5dd8721dfe9c5cba5048c0d0123
WOTTAKWOT <aywee26@gmail.com>
2022-07-13 22:29:57 +0300

    Move WeatherForecast to Application.

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Application;
 
@@ -6,6 +7,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        services.AddMediatR(typeof(DependencyInjection).Assembly);
         return services;
     }
 }
---
+++ src/Application/WeatherService/Queries/GetWeatherForecast.cs
@@ -0,0 +1,35 @@
+﻿using Domain;
+using MediatR;
+
+namespace Application.WeatherService.Queries;
+
+public record GetWeatherForecast() : IRequest<IEnumerable<WeatherForecast>>
+{
+    public class Handler : IRequestHandler<GetWeatherForecast, IEnumerable<WeatherForecast>>
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing",
+            "Bracing",
+            "Chilly",
+            "Cool",
+            "Mild",
+            "Warm",
+            "Balmy",
+            "Hot",
+            "Sweltering",
+            "Scorching"
+        };
+
+        public async Task<IEnumerable<WeatherForecast>> Handle(GetWeatherForecast request, CancellationToken cancellationToken)
+        {
+            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            }).ToArray();
+            return result;
+        }
+    }
+}
