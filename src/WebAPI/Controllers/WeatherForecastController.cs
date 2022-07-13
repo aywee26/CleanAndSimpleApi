d627e00eec069579a396e7d5eaa5bba2b392601c
WOTTAKWOT <aywee26@gmail.com>
2022-07-13 22:36:19 +0300

    Modify WeatherForecastController.

--- src/WebAPI/Controllers/WeatherForecastController.cs
+++ src/WebAPI/Controllers/WeatherForecastController.cs
@@ -1,3 +1,6 @@
+using Application.WeatherService.Queries;
+using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
@@ -6,28 +9,18 @@ namespace WebAPI.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        private readonly ISender _mediator;
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ISender mediator)
         {
-            _logger = logger;
+            _mediator = mediator;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpGet("GetWeatherForecast")]
+        public async Task<ActionResult<IEnumerable<WeatherForecast>>> Get(GetWeatherForecast request)
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            var result = await _mediator.Send(request);
+            return Ok(result);
         }
     }
 }
